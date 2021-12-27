e3aecdc57230b1679e43b69e9fab25a79539320b
brenoMatias <matiasbreno19@gmail.com>
2021-12-27 14:07:52 -0300

    loading modules

--- first-app/app.js
+++ first-app/app.js
@@ -1,14 +1,31 @@
-function sayHello(name) {
-    console.log('Hello' + name)
-}
+// function sayHello(name) {
+//     console.log('Hello' + name)
+// }
 
-sayHello('Breno');
-// sayHello(window);
+// sayHello('Breno');
+// // sayHello(window);
 
-console.log(module);
+// console.log(module);
 
 
 // in the terminal 
 // node app.js 
 // node give the file to v8 (js executer) for execution
 // in node we dont have window or document arguments -> just in browsers
+
+
+// Loading module
+
+
+const logger = require('./logger');
+// este e o modulo importado
+console.log(logger);
+
+logger.log1('message');
+// log is the function in logger
+
+
+// const logger2 = require('./logger');
+// logger2('breno test')
+// // logger2 is not an object because it was exported like 
+// //    module.exports = log2; instead module.exports.log2 = log2;
