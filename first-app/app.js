ca377abe9da2c87bc3499b5f42e1c3db08c7c0c4
brenoMatias <matiasbreno19@gmail.com>
2021-12-27 01:46:13 -0300

    first project

---
+++ first-app/app.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+    console.log('Hello' + name)
+}
+
+sayHello('Breno');
+
+// in the terminal 
+// node app.js 
+// node give the file to v8 (js executer) for execution
+// in node we dont have window or document arguments -> just in browsers
