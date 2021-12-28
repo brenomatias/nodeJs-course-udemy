ac6a6b3ecf24f9184ecb23f9b43efc7b452b3a16
brenoMatias <matiasbreno19@gmail.com>
2021-12-28 07:10:11 -0300

    creating routes

--- RESTful-API-Express/express-demo/index.js
+++ RESTful-API-Express/express-demo/index.js
@@ -14,7 +14,7 @@ app.get('/', (request, response) => {
 // is also called ROUTE HANDLER
 
 app.get('/api/courses', (req, res) => {
-   res.send()
+   res.send([1,2,3])
 });
 // '/api/courses' = endpoint
 
@@ -33,3 +33,33 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 //this is the proper way to assign a port.
 // To your Node applications. You should attempt to read the value
 // of an environment variable called port. If there is a value, we should use that.
+
+
+
+
+// implement a new route
+app.get('/api/courses/:id', (req, res) => {
+   res.send(req.params.id)
+})
+
+
+// NEWS BLOG
+// For example, imagine you're building a service for powering a
+// BLOG. So you could have a route like this, posts
+// year, month. So we have two parameters.
+// And with these you can get all the posts for even months
+
+app.get('/api/posts/:year/:month', (req, res) => {
+   res.send(req.params)
+})
+
+// this is our request params object. We have two properties
+//  year and month and their name based on our route parameters.
+
+//ex: 
+// http://localhost:5000/api/posts/2018/1
+// return: 
+// {
+//    "year": "2018",
+//    "month": "1"
+//  }
