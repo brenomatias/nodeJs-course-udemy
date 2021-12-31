72ac0e8c41bb9b8f4fc444bc20baddebe4e0ec77
brenoMatias <matiasbreno19@gmail.com>
2021-12-31 17:13:36 -0300

    callback function

--- ASYNCHRONOUS-JAVASCRIPT/async-demo/sync-async.js
+++ ASYNCHRONOUS-JAVASCRIPT/async-demo/sync-async.js
@@ -1,14 +1,23 @@
 console.log('Before');
+const user = getUser(1);
+console.log(user);
 console.log('After');
 
 //. In this program, when the first line executes the program is blocking, and the second line has to wait until the first line finishes execution. So that's why we call programs like this synchronous or blocking
 
 
 
+function getUser(id) { 
+    setTimeout(()=> {
+    console.log('Reading a usar from a database...');
+    return { id: id, gitHubUser: 'brenoMatias'};
+ }, 2000);
+     
+    return 1;
+}
 
-setTimeout(()=> {
-    console.log('Reading a usar from a database...')
-}, 2000)
+// const user = getUser(1);
+// console.log(user);   it will not work because the operation is async
 
 // simulating the action of reading a user from a database that is going to take two seconds
 
@@ -22,3 +31,5 @@ setTimeout(()=> {
 // seconds, next it will display this message in the console
 // and after that it will be free, so in two seconds from now
 // it will execute this function and display this message on the console.
+
+//  in Node programs, whenever you're dealing with an operation that involves disk or network access, you're dealing with asynchronous code
