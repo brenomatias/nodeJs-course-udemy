6849b2fdcfe48eae776f1dab837b8beec2674a34
brenoMatias <matiasbreno19@gmail.com>
2022-01-25 11:30:43 -0300

    built in validators

--- MONGOOSE-DATA-VALIDATION/index.js
+++ MONGOOSE-DATA-VALIDATION/index.js
@@ -4,13 +4,35 @@ mongoose.connect('mongodb://localhost/mongo-exercises');
 // connect to our MongoDB database
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: { 
+        type: String, 
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        // match: /pattern/
+    },
+    catetory: {
+        type: String,
+        enum: ['web', 'mobile', 'network']
+    },
     author: String, 
     tags: [ String ],
     date: { type:Date, default: Date.now }, 
     isPublished: Boolean,
-    price: Number
+    price: {
+       type: Number,
+       required: function() { return this.isPublished; },
+       min: 10,
+       max: 200,
+    }
   });
+
+  // required: true -> validation (booelean)
+  // required built in validators
+
+   // required: function() { return this.isPublished; }
+  // a function to conditionally make a property required.
+
 
 const Course = mongoose.model('Course', courseSchema);
 //create a model
@@ -19,16 +41,29 @@ async function createCourse() {
 
     const course = new Course({
         name: 'Back end course',
+        // catetory: ''
         author: 'Breno Matias',
         tags: ['nodejs', 'frontend'],
         isPublished: true,
+        price: 30,
     
     });
 
-    const result = await course.save();
-    console.log(result);
+    try {
+        const result = await course.save();
+        console.log(result);
+    }
+    catch (ex) {
+        console.log(ex.message);
+    }
+
 
 }
 
-
 createCourse();
+
+// In MongoDB we don't have that, MongoDB
+// doesn't care about any of this stuff. So this validation we have implemented here
+// is only meaningful in Mongoose. At the time we tried to save
+// a course, Mongoose runs the validation logic, and if the course,
+// is not valid it won't save it to the database
